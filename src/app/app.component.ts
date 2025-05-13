6fe870bdcb0830f78541197badefb896beebf4bd
nandini <nandani@contaque.com>
2025-05-13 12:31:14 +0530

    Add these changes
    Signed-off-by: nandini <nandani@contaque.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  userDetails:any;
   title = 'templatedriven24';
+  addDetails(val:NgForm){
+    console.log(val)
+    this.userDetails=val;
+  }
 }
